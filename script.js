028e824a678aa4d0720eeb88d7e028021921498b
kemillysouzaantunes-droid <kemilly.souza.antunes@escola.pr.gov.br>
2026-09-18 11:07:02 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const botoesCurtir = document.querySelectorALL(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+      let curtiu = false; 
+      botaoCurtir.addEventLstener("click", curtir);
+   function curtit() { 
+       const contador = botaoCurtir.querySelector("span");
+       if(curtiu === false){
+           contador.textContent++;
+           curtiu = true; }
+           else{
+                contador.textContent--;
+                curtiu = false;
+           }
+ }
+});
+
 
